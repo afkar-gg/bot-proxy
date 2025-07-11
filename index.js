7f4500ff3612765eaf11f3a0d3942a05b104896f
afkar-gg <afkargantengbetjir@gmail.com>
2025-07-11 21:26:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ if (!BOT_TOKEN || !CHANNEL) {
 const PORT = 3000;
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 const failedLogins = new Map();

2ba4c4833d161b3e7b610f0dfa11b7af530d46ae
afkar-gg <afkargantengbetjir@gmail.com>
2025-07-23 11:30:15 +0700

    Add files via upload

--- rblx.js
+++ rblx.js
@@ -511,9 +511,6 @@ app.post("/bond", async (req, res) => {
 
 
 // === /status (UI Page)
-const lastSeenAgo = Math.max(0, Date.now() - d.lastSeen);
-const lm = Math.floor(lastSeenAgo / 60000);
-const ls = Math.floor((lastSeenAgo % 60000) / 1000);
 app.get("/status", (req, res) => {
   res.send(`
 <!DOCTYPE html>
